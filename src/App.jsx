135a01392fb817efcafda1cf6c60c6efe39389d0
AbdulWaris009 <abdulwariidrisi11@gmail.com>
2025-11-14 11:02:27 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Navbar from './Components/Navbar'
+
+import FooterSuperFast from './Components/Footer'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Gallery from './pages/Gallery'
+import ServiceDetail from './pages/ServiceDetail'
+
+const App = () => {
+  return (
+   <BrowserRouter>
+   <Navbar/>
+   <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/about' element={<About/>} />
+    <Route path='/services/:slug' element={<ServiceDetail/>} />
+    <Route path='/gallery' element={<Gallery/>} />
+    <Route path='/contact' element={<Contact/>} />
+   </Routes>
+   <FooterSuperFast/>
+    </BrowserRouter>
+  )
+}
+
+export default App
